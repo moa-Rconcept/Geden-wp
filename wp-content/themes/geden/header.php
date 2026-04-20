350ba4f4cd560a8006cc572551da282e6a0d2e37
moa <yann.richert@neuf.Fr>
2026-04-21 00:47:52 +0200

    Update header.php

--- wp-content/themes/geden/header.php
+++ wp-content/themes/geden/header.php
@@ -37,7 +37,9 @@ if (is_page()) {
     <a class="brand" href="<?php echo esc_url(home_url('/')); ?>">
       <img src="<?php echo esc_url(get_template_directory_uri() . '/logo.png'); ?>" alt="Logo GeDEN" height="80">
       <div class="brand__title">
-        <div class="brand__tag"><?php bloginfo('description'); ?></div>
+        <div class="brand__tag">
+          <?php var_dump(get_bloginfo('description')); ?>
+        </div>
       </div>
     </a>
 
